dc37c75adaa936b191499f45956ad49e46b8a48c
charlesportoferreira <charlesportoferreira@gmail.com>
2019-03-07 10:28:56 -0500

    added stopwords to the preprocess step

--- wordvector/PreprocessUtil.py
+++ wordvector/PreprocessUtil.py
@@ -1,17 +1,24 @@
 import nltk as nltk
+from stop_words import get_stop_words
 
 nltk.download('punkt', quiet=True)
 
 
 class Preprocess:
+    stopWords = get_stop_words('en')  # list of stopwords/english PyPl
 
     def clean(self, text):
         tokens = self.tokenize(text)
         words_lowercase = self.lowercase(tokens)
-        return words_lowercase
+        words_after_stopremoval = self.removeStopWords(words_lowercase)
+        return words_after_stopremoval
 
     def lowercase(self, tokens):
         return [w.lower() for w in tokens]
 
     def tokenize(self, text):
         return nltk.word_tokenize(text)
+
+    def removeStopWords(self, words):
+        words = [w for w in words if w not in self.stopWords]
+        return words
